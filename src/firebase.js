ef71bdf889a6c12ffb6708b4031226a26d88c191
Songkail <songkr0314@gmail.com>
2023-04-06 23:29:37 +0900

    vertical

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,7 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/database';
 
 const firebaseConfig = {
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  databaseURL: "https://community-99db3-default-rtdb.firebaseio.com/",
 };
 
 firebase.initializeApp(firebaseConfig);
