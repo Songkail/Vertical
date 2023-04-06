64b8338cd516ef3657a4e7c88eca573bb8a04a4b
Songkail <songkr0314@gmail.com>
2023-04-06 22:12:18 +0900

    vertical

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import { database } from './firebase.js';
 
 function App() {
+  const [postContent, setPostContent] = useState('');
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const postsRef = database.ref('posts');
+    postsRef.on('value', (snapshot) => {
+      const postsData = snapshot.val();
+      const postsArray = Object.entries(postsData || {}).map(([key, value]) => ({ id: key, ...value }));
+      setPosts(postsArray);
+    });
+  }, []);
+
+  function addPost() {
+    if(document.getElementById("nickname-input").value !== '' && document.getElementById("write-input").value !== '') {
+      const newPostRef = database.ref('posts').push();
+      newPostRef.set({ content: document.getElementById("nickname-input").value + ' - ' + postContent });
+      setPostContent('');
+    }
+  }
+
+  function handleChange(event) {
+    setPostContent(event.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div id="nickname-wrapper">
+        <input type="text" id="nickname-input" placeholder="별명을 입력하세요. (실명 사용을 권유하지 않습니다.) " />
+      </div>
+
+      <div id="write-wrapper">
+        <textarea id="write-input" placeholder="내용을 입력하세요. " value={postContent} onChange={handleChange} />
+        <button id="write-reg" onClick={addPost}><b>등록</b></button>
+      </div>
+
+      <div id="post-wrapper">
+        {posts.map((post) => (
+          <div key={post.id} className="content">
+            <div id="post-content">{post.content}</div>
+          </div>
+        ))}
+      </div>
+
+      <a href='https://github.com/Songkail/Community'><div id="githubBtn-wrapper" style={{ width: "220px", height: "30px", position: "absolute", left: "5px", bottom: "25px"}}>
+        <div id="githubBtn-icon"></div>
+        <span>GitHub Repository</span>
+      </div></a>
+
+      <div style={{ position: "absolute", left: "5px", bottom: "5px"}}>
+        <b>📢 vertical 웹서비스는 개인개발 스튜디오인 Songkail Studio에서 코딩을 공부하고 있는 Songkail 학생이 운영이 아닌 백엔드 개발과 학습을 목적으로 개발한 사이트입니다. </b>
+        <span id="topBar-copyright" style={{color: "grey", marginLeft: "10px"}}>Copyright © 2023 Songkail Studio All rights reserved.</span>
+      </div>
+    </>
   );
 }
 
